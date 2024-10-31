556cf1a04fcf5a7f61485385790bf7b05324b59a
programacaoGuedess <karineflavia454@gmail.com>
2024-10-31 01:25:07 +0000

    novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,17 +1,46 @@
 #include <stdio.h>
 
 int main(){
-printf("Desafio super Trunfo, paises!\n");
+    printf("Desafio Super Trunfo! \n");
 
-int populacao;
-int pontosturisticos;
-float area;
-float PIB;
-char nome;
-char codigo;
+    int populacao;
+    int pontosturisticos;
+    float area;
+    float PIB;
+    char estado;
+    char cidade;
+    float codigo;
 
+printf("Digite o estado: \n");
+scanf("%s", &estado);
 
+printf("Digite a populacao: \n");
+scanf("%d", &populacao);
 
+printf("Digite pontos turisticos: \n");
+scanf("%e", &area);
 
+printf("Digite o PIB: \n");
+scanf("%f", &PIB);
 
+printf("Digite a cidade: \n");
+scanf("%s", &cidade);
+
+printf("Digite codigo da carta: \n");
+scanf("%e", &codigo);
+
+return 0;     
 }
+
+ 
+    
+
+
+
+
+
+
+
+
+
+

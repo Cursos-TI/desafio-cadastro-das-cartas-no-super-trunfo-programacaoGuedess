4245395d686d63fe184565dd1c477ab09f6f760a
programacaoGuedess <karineflavia454@gmail.com>
2024-11-02 02:36:17 +0000

    commit concluido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,37 +1,155 @@
 #include <stdio.h>
 
 int main(){
-    printf("Desafio Super Trunfo! \n");
+printf("Desafio Super trunfo!\n");
 
-    int populacao;
-    int pontosturisticos;
-    float area;
-    float PIB;
-    char estado;
-    char cidade;
-    float codigo;
+char estado = 'A';
+char codigo = "A01";
+char cidade[20] = "sao paulo";
+int populacao = 12322000;
+float area = 1521.11;
+float PIB = 966.55;"bilhoes de reais";
+int pontos_turisticos = 50;
 
-printf("Digite o estado: \n");
-scanf("%s", &estado);
-
-printf("Digite a populacao: \n");
-scanf("%d", &populacao);
-
-printf("Digite pontos turisticos: \n");
-scanf("%e", &area);
-
-printf("Digite o PIB: \n");
-scanf("%f", &PIB);
-
-printf("Digite a cidade: \n");
+printf("Estado: %c\n", estado);
+scanf("%c", &estado);
+printf("Codigo: %d\n", codigo);
+scanf("%d", &codigo);
+printf("Cidade: %s\n", cidade);
 scanf("%s", &cidade);
+printf("Populacao: %d\n", populacao);
+scanf("%d", &populacao);
+printf("Area: %f\n", area);
+scanf("%f", &area);
+printf("PIB: %f\n", PIB);
+scanf("%f", &PIB);
+printf("Pontos_turisticos: %d\n", pontos_turisticos);
+scanf("%d", &pontos_turisticos);
 
-printf("Digite codigo da carta: \n");
-scanf("%e", &codigo);
+return 0;
 
-return 0;     
-}
+ }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+ 
+
+    
  
     
 
